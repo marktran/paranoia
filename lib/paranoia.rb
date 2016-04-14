794e5ddc92f52cd9267f41ba18b52a0a44e6ba54
Mark Tran <mark.tran@gmail.com>
2016-04-15 03:26:04 +0700

    remove patch

--- lib/paranoia.rb
+++ lib/paranoia.rb
@@ -259,24 +259,3 @@ class ActiveRecord::Base
 end
 
 require 'paranoia/rspec' if defined? RSpec
-
-module ActiveRecord
-  module Validations
-    module UniquenessParanoiaValidator
-      def build_relation(klass, table, attribute, value)
-        relation = super(klass, table, attribute, value)
-        return relation unless klass.respond_to?(:paranoia_column)
-        arel_paranoia_scope = klass.arel_table[klass.paranoia_column].eq(klass.paranoia_sentinel_value)
-        if ActiveRecord::VERSION::STRING >= "5.0"
-          relation.where(arel_paranoia_scope)
-        else
-          relation.and(arel_paranoia_scope)
-        end
-      end
-    end
-
-    class UniquenessValidator < ActiveModel::EachValidator
-      prepend UniquenessParanoiaValidator
-    end
-  end
-end
